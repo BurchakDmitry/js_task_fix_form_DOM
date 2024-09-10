ba8f914db648addcbb47119f9809bb81b18b4e78
Burchak Dmitry <burchak.dmitry@icloud.com>
2024-09-10 13:54:18 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const fields = [
+    ...document.querySelectorAll(".field")
+];
+fields.forEach((field)=>{
+    const input = field.querySelector("input");
+    const label = document.createElement("label");
+    label.classList.add("field-label");
+    label.setAttribute("for", `${input.id}`);
+    label.textContent = getCorrectName(input.getAttribute("name"));
+    input.setAttribute("placeholder", getCorrectName(input.name));
+    field.insertBefore(label, input);
+});
+function getCorrectName(unCorrectName) {
+    let correctName = "";
+    for (const ch of unCorrectName){
+        if (ch === ch.toUpperCase()) correctName += " ";
+        correctName += ch;
+    }
+    return correctName.toLocaleLowerCase();
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
